40f077dc4e00e84e6c42b901a74756a467147432
DeLaCruz26 <dametrik.fick@gmail.com>
2020-11-26 00:10:37 -0500

    added nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :techs  do 
+    resources :repairs, only: [:index, :new, :create]
+  end
+
+  resources :customers do 
+    resources :repairs, only: [:index]
+  end
 end
