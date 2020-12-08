d0649c84f85355da8690b366b88edb1a19abf91e
DeLaCruz26 <dametrik.fick@gmail.com>
2020-12-08 15:54:18 -0500

    deleted unused methods

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -5,29 +5,5 @@ class DevicesController < ApplicationController
     @devices = Device.alpha
    end
    
-   def new
-      @device = Device.new
-   end
-
-   def create
-      @device = Device.new(device_params)
-      @device.user_id = session[:user_id]
-      if @device.save
-          flash[:message] = "Device Added!"
-          redirect_to device_path(@device)
-      else 
-          render :new 
-      end
-  end
-
-   def show
-      @device = Device.find_by(params[:id])   
-   end
-
-  private
-
-   def device_params
-      params.require(:device).permit(:name, :model, :user_id)
-   end
 
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,5 +1,4 @@
 class Device < ApplicationRecord
-    belongs_to :user
     has_many :repairs
     validates :name, presence: true, uniqueness: true
     validates :model, presence: true, uniqueness: true
